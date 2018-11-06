4dce9d01295afb04bd3c3ec67ec7f2db8408687d
pierrejego <pierre.jego@jdev.fr>
2018-11-06 14:05:30 +0100

    Create specific plugin for georchestra authentification system

---
+++ core/src/plugins/authfront.http_georchestra/i18n/conf/en.php
@@ -0,0 +1,15 @@
+<?php
+$mess=array(
+"geOrchestra authentication" => "geOrchestra authentication",
+"Auth performed by geOrchestra security-proxy" => "Auth performed by geOrchestra security-proxy",
+"Create User" => "Create User",
+"Automatically create user if it does not already exists" => "Automatically create user if it does not already exists",
+"Admin login" => "Admin login",
+"Automatically set this login as pydio administrator" => "Automatically set this login as pydio administrator",
+"Order" => "Order",
+"Is Pydio instalr behind several securty-proxy (ex intranet + extranet)" => "Is Pydio instalr behind several securty-proxy (ex intranet + extranet)",
+"Order this plugin with other auth frontends" => "Order this plugin with other auth frontends",
+"Value of X-Forwarded-Server for extranet" => "Value of X-Forwarded-Server for extranet",
+"Value of X-Forwarded-For for extranet" => "Value of X-Forwarded-For for extranet",
+"Value of extranet prefix for example e_" => "Value of extranet prefix for example e_",
+);
---
+++ core/src/plugins/authfront.http_georchestra/i18n/conf/fr.php
@@ -0,0 +1,15 @@
+<?php
+$mess=array(
+"geOrchestra authentication" => "geOrchestra authentification",
+"Auth performed by geOrchestra security-proxy" => "Auth via geOrchestra security-proxy",
+"Create User" => "Créer un utilisateur si il n'existe pas",
+"Automatically create user if it does not already exists" => "Creer les utilisateurs si ils n'existent pas",
+"Admin login" => "Login du compte d'administrateur pour l'affecter lors de la première connexion",
+"Automatically set this login as pydio administrator" => "Mettre cet utilisateur en administrateur",
+"Order" => "Ordre de priorité de ce système d'authentification",
+"Is Pydio instalr behind several securty-proxy (ex intranet + extranet)" => "Est-ce Pydio est installer derrière plus d'un security-proxy",
+"Order this plugin with other auth frontends" => "Ordre de ce pluggin par rapport aux autres systèmes d'authentification",
+"Value of X-Forwarded-Server for extranet" => "Valeur du header X-Forwarded-Server pour l'extranet",
+"Value of X-Forwarded-For for extranet" => "Valeur du header X-Forwarded-For pour l'extranet",
+"Value of extranet prefix for example e_" => "Valeur du prefix pour le nom de l'utilisateur extranet par exemple e_",
+);
